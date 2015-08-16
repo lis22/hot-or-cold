8ec897b84dd11248fe095b9e304812b106e0867b
lis22 <lisieness@gmail.com>
2015-08-16 12:15:31 -0400

    fixed newgame problem

--- js/app.js
+++ js/app.js
@@ -1,151 +1,130 @@
-//Global Variables
-window.secretNum;
-window.guessCount=0;
-
 $(document).ready(function(){
 
+  var secretNum = getRandomNumber();
+  var guessCount=0;
+
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000); 
-  	});
-
-		newGame();
-
-		$(".new").click(function(e) {
-			//reset text, textbox and button
-			$("#feedback").text("Make your guess");
-			$("#userGuess").prop("disabled", false);
-			$("#guessButton").prop("disabled", false);
-
-			resetCount();
-			removeListItems();
-			newGame();
-		});
-});
-
-/*Starts a new game, gets random number, gets the user's guess, and sends
-guess and random number to get feedback */
-function newGame() {
-
-	window.secretNum = getRandomNumber();
-
-	$("form").submit(function(e) {
-		 var inputNumber;
-
-		 e.preventDefault();
-
-		 inputNumber = getInput();
-
-		 //continue on with valid input only
-		 if (inputNumber !=-1) {
-		 	increaseCount();
-
-		 	addGuessToList(inputNumber);
-
-		 	getFeedback(inputNumber, window.secretNum);
-		}
-
+  $(".what").click(function(){
+  $(".overlay").fadeIn(1000);
 	});
-}
 
-/* get and validate user input and returns */
-function getInput() {
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  });
 
-	guessNum = parseInt($("#userGuess").val());
+  /*Start a new game when user clicks new game */
+  $(".new").click(function(e) {
+		newGame();
+  });
 
-	if (guessNum >= 1 && guessNum <= 100){
-		$("#userGuess").val('');
-		return guessNum;
-	}
+  /*Resets text, buttons, list, count, and gets a new random number */
+  function newGame() {
 
-	else {
-		$("#feedback").text("Please be sure to enter a valid number");
-		$("#userGuess").val('');
-		return -1;
-	}
-}
+    secretNum=getRandomNumber();
+    guessCount=0;
 
-/*Generates a random number between 1 & 100*/
-function getRandomNumber(){
-	var randomNum;
-	var min= 1;
-	var max= 100;
+    $("#feedback").text("Make your guess");
+    $("#count").text(guessCount);
+    $("#userGuess").prop("disabled", false);
+    $("#guessButton").prop("disabled", false);
+    $("#guessList li").remove();
+  }
 
-	randomNum = Math.floor((Math.random() * max) + min);
-	console.log("Random number is: " + randomNum);
+  /*Get data when submitted from the form and sends it to be processed further*/
+  $("form").submit(function(e) {
 
-	return randomNum;
-}
+    e.preventDefault();
 
-/**The user gets feedback about each guess to help them figure out what the random
-generated number is */
-function getFeedback(guess, answer) {
+    var inputNumber;
+    inputNumber = getInput();
 
-	var previousGuess = $("#guessList li").last().prev().text();
-	var guessDifference = Math.abs(guess-answer);
+    //continue on with valid numeric input only
+    if (inputNumber !=-1) {
 
-	//game over so disables textbox and button
-	if (guessDifference==0) {
-		$("#feedback").text("You won! The answer is " + window.secretNum);
-		$("#userGuess").prop("disabled", true);
-		$("#guessButton").prop("disabled", true);
-	}
+      //increase count and display
+      guessCount++;
+      $("#count").text(guessCount);
 
-	/*previous guess exists so bases hint on last given guess.
-	This was suggested as a more advanced feature */
-	else if($.isNumeric(previousGuess)) {
-		var previousDifference = Math.abs(previousGuess-answer);
+      //add guess to list
+      $("#guessList").append("<li>" + inputNumber + "</li> ");
 
-		if(guessDifference > previousDifference)
-			$("#feedback").text("Colder");
-		else
-			$("#feedback").text("Warmer");
-	}
+      //send input and random number to determine proper feedback to show
+      getFeedback(inputNumber, secretNum);
+    }
+  });
 
-	//This executes on the user's first guess only
-	else {
-		if(guessDifference >= 50)
-			$("#feedback").text("Ice Cold");
+  /* get and validate user input and returns */
+  function getInput() {
 
-		else if(guessDifference > 30 && guessDifference < 50)
-			$("#feedback").text("Cold");
+  	guessNum = parseInt($("#userGuess").val());
+    console.log("User guess:" + guessNum);
 
-		else if(guessDifference > 20 && guessDifference <= 30)
-			$("#feedback").text("Warm");
+  	if (guessNum >= 1 && guessNum <= 100){
+  		$("#userGuess").val('');
+  		return guessNum;
+  	}
 
-		else if(guessDifference > 10 && guessDifference <= 20)
-				$("#feedback").text("Hot");
+  	else {
+  		$("#feedback").text("Please be sure to enter a valid number");
+  		$("#userGuess").val('');
+  		return -1;
+  	}
+  }
 
-		else if(guessDifference >= 1 && guessDifference <=10)
-				$("#feedback").text("Very Hot");
-	}
-}
+  /*Generates a random number between 1 & 100 and returns it*/
+  function getRandomNumber(){
+  	var randomNum;
+  	var min= 1;
+  	var max= 100;
 
-/** supplies users with a list of the numbers they have guessed so far. **/
-function addGuessToList(guess) {
-	  $("#guessList").append("<li>" + guess + "</li> ");
-}
+  	randomNum = Math.floor((Math.random() * max) + min);
+  	console.log("Random number is: " + randomNum);
 
-/** Remove guess list items **/
-function removeListItems() {
-  if ($("#guessList li").length > 0)
-    ($("#guessList li").remove());
-}
+  	return randomNum;
+  }
 
-/**Increases count and displays on screen **/
-function increaseCount() {
-	window.guessCount++;
-	$("#count").text(window.guessCount);
-}
+  /**The user gets feedback about each guess to help them figure out what the random
+  generated number is */
+  function getFeedback(guess, answer) {
 
-/** Resets the counter and resets the text to display on screen **/
-function resetCount() {
-	window.guessCount=0;
-	$("#count").text(window.guessCount);
-}
+  	var previousGuess = $("#guessList li").last().prev().text();
+  	var guessDifference = Math.abs(guess-answer);
+
+  	//game over so disables textbox and button
+  	if (guessDifference==0) {
+  		$("#feedback").text("You won! The answer is " + secretNum);
+  		$("#userGuess").prop("disabled", true);
+  	  $("#guessButton").prop("disabled", true);
+  	}
+
+  	/*previous guess exists so bases hint on last given guess.
+  	This was suggested as a more advanced feature */
+  	else if($.isNumeric(previousGuess)) {
+  		var previousDifference = Math.abs(previousGuess-answer);
+
+  		if(guessDifference > previousDifference)
+  			$("#feedback").text("Colder");
+  		else
+  			$("#feedback").text("Warmer");
+  	}
+
+  	//This executes on the user's first guess only
+  	else {
+  		if(guessDifference >= 50)
+  			$("#feedback").text("Ice Cold");
+
+  		else if(guessDifference > 30 && guessDifference < 50)
+  			$("#feedback").text("Cold");
+
+  		else if(guessDifference > 20 && guessDifference <= 30)
+  			$("#feedback").text("Warm");
+
+  		else if(guessDifference > 10 && guessDifference <= 20)
+  				$("#feedback").text("Hot");
+
+  		else if(guessDifference >= 1 && guessDifference <=10)
+  				$("#feedback").text("Very Hot");
+  	}
+  }
+});
